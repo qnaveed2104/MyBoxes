a99e87d069a723b7c6cec6bbcd1cc7d817ba5b1a
qazi <qazi.naveed@nextgeni.com>
2024-12-31 14:18:00 +0100

    example app

---
+++ dummy/dummy/dummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyApp.swift
+//  dummy
+//
+//  Created by Qazi on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct dummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dummy/dummyTests/dummyTests.swift
@@ -0,0 +1,17 @@
+//
+//  dummyTests.swift
+//  dummyTests
+//
+//  Created by Qazi on 31/12/2024.
+//
+
+import Testing
+@testable import dummy
+
+struct dummyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
